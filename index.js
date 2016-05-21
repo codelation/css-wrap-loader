a2ecb90c19500c0ede5e3216954295e997f3330d
Brian Pattison <brian@brianpattison.com>
2016-05-21 15:14:20 -0500

    Only load Sass files in a views directory

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 module.exports = function(source) {
   this.cacheable();
 
+  if (this.resource.split('/').indexOf('views') < 0) {
+    return source;
+  }
+
   var viewName = this.resource.split('/')
                               .slice(-3)
                               .slice(0, 2)
